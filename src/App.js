d7d29940a29b52b5a399105d664815f9082caf19
Calo10 <carlosmendez@LAAPJG5H302-CarlosMendez.local>
2018-10-08 09:48:53 -0600

    before terminal change

--- src/App.js
+++ src/App.js
@@ -81,11 +81,22 @@ class App extends Component {
         </div> 
         );
 
+        style.backgroundColor = 'red';
+
+    }
+
+    let classes = [];
+    if(this.state.persons.length < 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>hi Im Carlos</h1>
+        <p className={classes.join(' ')}>This is really working</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle</button>
